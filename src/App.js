738f09051be79948d49fa205f278a050dabe162d
sourabhch13 <sourabh.c@consultadd.com>
2024-01-16 14:50:58 +0530

    Init and Basic Routing:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route,Routes,Link} from 'react-router-dom';
+import Department from './components/Department';
+import Employee from './components/Employee';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<h1>CRUD Application</h1>
+				<ul className='navList'>
+					<li><Link to='/' className='link'>Employee</Link></li>
+					<li><Link to='/department' className='link'>Department</Link></li>
+				</ul>
+				<Routes>
+					<Route path='/' element={<Employee/>} />
+					<Route path ='/department' element={<Department/>} />
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/Department.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Department = () => {
+  return (
+    <h1>department</h1>
+  )
+}
+
+export default Department;
